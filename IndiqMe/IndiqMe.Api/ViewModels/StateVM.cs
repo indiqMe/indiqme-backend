a250985f81098c63b104512a6b69ae2da2e350b8
Daniel Saraiva <danniel.saraiva@gmail.com>
2019-05-27 21:15:18 -0300

    Conforme solicitado foi consumido a API do IBGE
    Conforme solicitado foi consumido a API do IBGE e criado um endpoint na API do INDIQME. Feito esse desenvolvimento para listar os estados e cidades na hora do cadastro.

---
+++ IndiqMe/IndiqMe.Api/ViewModels/StateVM.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace IndiqMe.Api.ViewModels
+{
+    public class StateVM
+    {
+
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("sigla")]
+        public string Initials { get; set; }
+
+        [JsonProperty("nome")]
+        public string Name { get; set; }
+
+        [JsonProperty("regiao")]
+        public Region Region { get; set; }
+    }
+
+    public class Region
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("sigla")]
+        public string Initials { get; set; }
+
+        [JsonProperty("nome")]
+        public string Name { get; set; }
+    }
+
+}
---
+++ IndiqMe/IndiqMe.Service/RequestRestSharp/IGenericRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using RestSharp;
+using System.Collections.Generic;
+
+namespace IndiqMe.Service.RequestRestSharp
+{
+    public interface IGenericRequest
+    {
+        string ReturnJson(Dictionary<string, string> Headers, Dictionary<string, string> Param, Method MethodType, string url);
+
+    }
+}
